5ed693b4e763a83851a2a732ec82144e72880ab0
ndenicolais <n.denicolais@arethusasrl.it>
2022-06-16 12:37:51 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import Footer from './components/footer';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+      <App />
+      <Footer />
+    </React.StrictMode>
+  );
